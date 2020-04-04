7dbf632d0aff818a17e609750d40067f1b7580b6
bitsnaps <bitsnaps@yahoo.fr>
2020-04-04 22:21:34 +0100

    first example: a simple ReQL

--- src/main/groovy/Hello.groovy
+++ src/main/groovy/Hello.groovy
@@ -1,6 +1,9 @@
 
 import io.vertx.core.AbstractVerticle
 import io.vertx.core.Vertx
+import com.rethinkdb.RethinkDB
+import com.rethinkdb.net.Connection
+import com.rethinkdb.net.Cursor
 
 class Hello  {
 
@@ -9,15 +12,11 @@ class Hello  {
     }
 
 }
-
 class Server extends AbstractVerticle {
-    void start(){
-        def vertx = Vertx.vertx()
-        vertx.createHttpServer().requestHandler({ req ->
-          req.response()
-            .putHeader("content-type", "text/html")
-            .end("<html><body><h1>Hello from vert.x!</h1></body></html>")
-        }).listen(8080)
-    }
 
+  void start(){
+    RethinkDB r = RethinkDB.r
+    Connection conn = r.connection().hostname("localhost").port(28015).connect()
+    println(r.range(10).coerceTo("array").run(conn))
+  }
 }
