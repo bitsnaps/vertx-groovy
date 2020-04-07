15839af958d209da45727b659a0e3db26a8e39f0
bitsnaps <bitsnaps@yahoo.fr>
2020-04-07 22:22:20 +0100

    add a simple router

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -5,33 +5,10 @@ import io.vertx.ext.web.Router;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Date;
 import java.util.function.Consumer;
 
 public class Hello extends AbstractVerticle {
-
-/*
-  // https://knative.dev/community/samples/serving/helloworld-vertx/
-  public void start() {
-
-         final HttpServer server = vertx.createHttpServer();
-         final Flowable<HttpServerRequest> requestFlowable = server.requestStream().toFlowable();
-
-         requestFlowable.subscribe(httpServerRequest -> {
-
-             String target = System.getenv("TARGET");
-             if (target == null) {
-                 target = "NOT SPECIFIED";
-             }
-
-             httpServerRequest.response().setChunked(true)
-                     .putHeader("content-type", "text/plain")
-                     .setStatusCode(200) // OK
-                     .end("Hello World: " + target);
-         });
-
-         server.listen(8080);
-     }
-*/
 
 //    public static void main(String[] args){
 //    }
@@ -40,18 +17,14 @@ public class Hello extends AbstractVerticle {
     public void start(Future<Void> fut) throws Exception {
 
         Router router = Router.router(vertx);
-//        router.route().handler(req ->
-//            req.response()
-//                    .putHeader("content-type", "text/html")
-//                    .end("<html><body><h1>Hello from vert.x!</h1></body></html>")
-//        );
 
-        vertx.createHttpServer().requestHandler(req -> {
+        router.route().handler(req ->
             req.response()
                     .putHeader("content-type", "text/html")
-                    .end("<html><body><h1>Hello from vert.x!</h1></body></html>");
+                    .end("<html><body><h1>Hello vert.x!</h1><p>"+new Date().toString()+"</p></body></html>")
+        );
 
-        }).listen(8080, result -> {
+        vertx.createHttpServer().requestHandler(handler -> router.accept(handler)).listen(8080, result -> {
             if (result.succeeded()) {
                 fut.complete();
             } else {
