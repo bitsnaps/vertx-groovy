6c92d1dac7b6dbf151f04e5a376690f71be88ba1
bitsnaps <bitsnaps@yahoo.fr>
2020-04-07 15:24:04 +0100

    first commit (rxjava)

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -10,17 +10,6 @@ import java.io.File;
 import java.io.IOException;
 import java.util.function.Consumer;
 
-/*/ Way 1
-public class Hello {
-
-    public static void main(String[] args) {
-        Vertx.vertx().createHttpServer().requestHandler(req ->
-                req.response()
-                        .putHeader("content-type", "text/html")
-                        .end("<html><body><h1>Hello from vert.x!</h1><br><p>Date: " + DateHelper.today() + "</p></body></html>")
-        ).listen(8080);
-    }
-}*/
 
 // Way 2
 public class Hello extends AbstractVerticle {
@@ -51,55 +40,3 @@ public class Hello extends AbstractVerticle {
         );
     }
 }
-
-/*/ Way 3
-public class Hello {
-
-    public static void main(String[] args) {
-
-        Vertx vertx = Vertx.vertx();
-        Router router = Router.router(vertx);
-        // set method for a route
-//        Route route = Router.router(vertx).route().method(HttpMethod.GET);
-
-        // handler any request
-//        router.route().handler(req ->
-        // handle get "/" request
-        router.route("/:name")
-//                .consumes("application/json") // consume only messages of type json (you can replace 'application' by '*'
-                .handler( reqCtx -> {
-            HttpServerRequest req = reqCtx.request();
-            String name = req.getParam("name").isEmpty()?"User":req.getParam("name");
-            reqCtx.response()
-                    .putHeader("content-type", "text/html")
-                    .end("<html><body><h1>Welcome "+name+"</h1><h1>Hello vert.x!</h1><p>"+DateHelper.today()+"</p></body></html>");
-    });
-
-
-        // Multiple handlers
-//        Route hanlder1 = router.get("/").handler(req -> {
-//            req.response()
-//                .setChunked(true) // stream the data (reactive)
-//                .write("<h1>Welcome "+name+", Please wait, processing...</h1>");
-//            req.vertx().setTimer(3000, tid -> {
-//                req.next();
-//            });
-//        });
-//
-//        Route hanlder2 = router.get("/").handler(req -> {
-//            req.response()
-//                .write("<h2>You're close, almost done...</h2>");
-//            req.vertx().setTimer(2000, tid -> {
-//                req.next();
-//            });
-//        });
-//
-//        Route hanlder3 = router.get("/").handler(req -> {
-//            req.response()
-//                .end("<h3>Thank your for waiting.</h3>");
-//        });
-
-        vertx.createHttpServer().requestHandler(router::accept).listen(8080);
-    }
-}
-*/
